f111e2e731d0358a9e55725877071cfa681765da
BigPlayBoy <1151770629@qq.com>
2016-06-18 22:13:03 +0800

    6.18

---
+++ src/com/imooc/action/HelloWorldAction.java
@@ -0,0 +1,15 @@
+package com.imooc.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldAction extends ActionSupport {
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return super.execute();
+	}
+
+	
+	
+}
