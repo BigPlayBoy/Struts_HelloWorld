0e176bc0d7ff4f0d9c93d68e98b2ae217bdb0749
BigPlayBoy <1151770629@qq.com>
2016-06-19 10:37:49 +0800

    6.19 网页打不开 action有用

--- src/com/imooc/action/HelloWorldAction.java
+++ src/com/imooc/action/HelloWorldAction.java
@@ -8,7 +8,7 @@ public class HelloWorldAction extends ActionSupport {
 	@Override
 	public String execute() throws Exception {
 		System.out.println("执行Action");
-		return SUCCESS;
+		return "11";
 		
 	}
 
